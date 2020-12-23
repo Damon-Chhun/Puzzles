aede1c2fe33d1cfc58acce8664713df560d7e712
Damon Chhun <damonchhun7@gmail.com>
2020-12-23 15:03:43 -0800

    establishing test routes

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API running"));
 
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/reviews", require("./routes/api/reviews"));
+app.use("/api/shop", require("./routes/api/shop"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
