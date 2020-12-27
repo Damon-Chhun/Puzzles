d7aa8d820abae1bcb372d5bf54b78191f454c5a9
Damon Chhun <damonchhun7@gmail.com>
2020-12-27 11:37:32 -0800

    first npm script to add a user reated with faker.js to mongoDB

--- faker/users/registerUsers.js
+++ faker/users/registerUsers.js
@@ -1,6 +1,9 @@
 const faker = require("faker");
 const User = require("../../models/User");
 const gravatar = require("gravatar");
+const MongoClient = require("mongodb");
+const config = require("config");
+const url = config.get("mongoURI");
 
 const email = faker.internet.email();
 const avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
@@ -13,5 +16,14 @@ user = new User({
 });
 
 console.log("HELLLOOOOO!!!!");
+console.log(user);
 
-console.log("HELLLOOOOO!!!!");
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  const dbo = db.db("Puzzles");
+  dbo.collection("users").insertOne(user, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});
