9ff5f7949c041eccdee4127d76179c2e5cb18aea
Damon Chhun <damonchhun7@gmail.com>
2021-01-02 14:34:13 -0800

    fixing name of files, seed mensClothes

--- seed/seed-mens.js
+++ seed/seed-mens.js
@@ -10,31 +10,31 @@ const menClothes = [];
 const { menClothesNames, menClothesImageUrls, menClothesPrices } = shopData;
 
 for (var i = 0; i < shopData.menClothesNames.length; ++i) {
-  let menClothes = {
+  let menClothe = {
     title: menClothesNames[i],
     imageURL: menClothesImageUrls[i],
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quia, non repellat vel praesentium itaque omnis animi earum quam perspiciatis minima ipsum et quibusdam asperiores nostrum labore autem! Explicabo, in.",
     price: menClothesPrices[i]
   };
-  menClothess.push(menClothes);
+  menClothes.push(menClothe);
 }
 
-console.log(menClothess);
+// console.log(menClothes);
 
 shop = new Shop({
   Department: "menClothes",
   product: menClothes
 });
 
-console.log(shop);
+// console.log(shop);
 
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   const dbo = db.db("Puzzles");
-//   dbo.collection("shop").insertOne(shop, function(err, res) {
-//     if (err) throw err;
-//     console.log(`${i} of documents inserted into users collection`);
-//     db.close();
-//   });
-// });
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  const dbo = db.db("Puzzles");
+  dbo.collection("shop").insertOne(shop, function(err, res) {
+    if (err) throw err;
+    console.log(`${i} of documents inserted into users collection`);
+    db.close();
+  });
+});
