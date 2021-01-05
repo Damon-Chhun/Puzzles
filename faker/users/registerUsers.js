412daa2f4784e2ecf9add5c8a030e4315433bc15
Damon Chhun <damonchhun7@gmail.com>
2021-01-04 21:45:26 -0800

    login user route and fixed register npm script so now it hashes the passwords before stored in mongoDB

--- faker/users/registerUsers.js
+++ faker/users/registerUsers.js
@@ -6,50 +6,60 @@ const MongoClient = require("mongodb");
 const config = require("config");
 const url = config.get("mongoURI");
 const jsonwebtoken = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 let users = [];
 let counter = 0;
 
-while (counter < 10) {
-  let email = faker.internet.email();
-  let avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
+const seedUsers = async () => {
+  while (counter < 10) {
+    let email = faker.internet.email();
+    let avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
 
-  user = new User({
-    name: faker.name.firstName() + " " + faker.name.lastName(),
-    email,
-    avatar,
-    password: faker.internet.password()
-  });
+    user = new User({
+      name: faker.name.firstName() + " " + faker.name.lastName(),
+      email,
+      avatar,
+      password: faker.internet.password()
+    });
 
-  users.push(user);
-  ++counter;
-}
+    console.log(user.password);
 
-console.log(users);
+    const salt = await bcrypt.genSalt(10);
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  const dbo = db.db("Puzzles");
-  dbo.collection("users").insertMany(users, function(err, res) {
-    if (err) throw err;
-    console.log(`${counter} of documents inserted into users collection`);
-    db.close();
-  });
-});
+    user.password = await bcrypt.hash(user.password, salt);
 
-const payload = {
-  user: {
-    id: user.id
+    users.push(user);
+    ++counter;
   }
+
+  console.log(users);
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    const dbo = db.db("Puzzles");
+    dbo.collection("users").insertMany(users, function(err, res) {
+      if (err) throw err;
+      console.log(`${counter} of documents inserted into users collection`);
+      db.close();
+    });
+  });
+
+  const payload = {
+    user: {
+      id: user.id
+    }
+  };
+
+  jsonwebtoken.sign(
+    payload,
+    config.get("jwtSecret"),
+    { expiresIn: 3600 },
+    (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    }
+  );
 };
 
-jsonwebtoken.sign(
-  payload,
-  config.get("jwtSecret"),
-  { expiresIn: 3600 },
-  (err, token) => {
-    console.log(token);
-    if (err) throw err;
-    res.json({ token });
-  }
-);
+seedUsers();
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,18 +2,81 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
+const { check, validationResult } = require("express-validator");
+const jsonwebtoken = require("jsonwebtoken");
+const config = require("config");
+const bcrypt = require("bcryptjs");
 
 //@route    GET api/auth
 //@desc      Test route
-//@access   Public
+//@access   Private
 router.get("/", auth, async (req, res) => {
   try {
-    const user = User.findById(req.user.id).select("-password");
-    await res.json(user);
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
   } catch (err) {
-    console.err(err.message);
+    console.error(err.message);
     res.status(500).send("Server Error");
   }
 });
+
+//@route    POST api/auth
+//@desc     Login a user
+//@access   Private
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is invalid").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      console.log(user);
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jsonwebtoken.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.rror(error.essage);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
