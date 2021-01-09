d19a7c88b7c49ed3a1b9e29fe6a0e1404291b7b2
Damon Chhun <damonchhun7@gmail.com>
2021-01-09 13:14:35 -0800

    cart route complete

--- routes/api/cart.js
+++ routes/api/cart.js
@@ -31,20 +31,34 @@ router.post(
     console.log(req.body);
 
     const { productID, quantity, name, price } = req.body;
-    const { userId } = req;
+    const { id } = req.user;
+    console.log(id);
     try {
-      let doesCartExist = await Cart.findOne({ userId });
+      let doesCartExist = await Cart.findOne({ userId: id });
 
-      // If there is already a cart that exists
+      if (!doesCartExist && quantity < 0) {
+        // If trying to reduce a product in a cart that doesn't exist
+        return res.status(400).json({ msg: "No Cart Exist" });
+      }
+
       if (doesCartExist) {
+        // If there is already a cart that exists
         let doesItemExist = doesCartExist.products.findIndex(
           product => product.productID == productID
         );
+
+        if (doesCartExist && quantity < 0 && doesItemExist < 0) {
+          // If trying to reduce a product in a cart that doesn't exist
+          return res.status(400).json({ msg: "No Item Exist" });
+        }
 
         if (doesItemExist > -1) {
           //Item already exists in cart (change quantity)
           doesCartExist.products[doesItemExist].quantity += quantity;
           console.log(doesCartExist.products[doesItemExist].quantity);
+          if (doesCartExist.products[doesItemExist].quantity <= 0) {
+            doesCartExist.products.splice(doesItemExist, 1);
+          }
         } else {
           //Item doesnt exists in cart
           doesCartExist.products.push({
@@ -61,7 +75,7 @@ router.post(
       //build Cart object (No Cart exists)
       else {
         let reqCart = new Cart({
-          userId,
+          userId: id,
           products: [
             {
               productID,
@@ -81,5 +95,79 @@ router.post(
     }
   }
 );
+
+//@route    POST api/shop
+//@desc     Reduce quantity of an item
+//@access   Private
+// router.post(
+//   "/",
+//   [
+//     auth,
+//     [
+//       check("productID", "Invalid productID").exists(),
+//       check("quantity", "No quantity provided").isNumeric()
+//     ]
+//   ],
+//   async (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
+
+//     console.log(req.body);
+
+//     const { productID, quantity } = req.body;
+//     const { id } = req.user;
+//     try {
+//       let doesCartExist = await Cart.findOne({ userId: id });
+
+//       // If there is already a cart that exists
+//       if (doesCartExist) {
+//         let doesItemExist = doesCartExist.products.findIndex(
+//           product => product.productID == productID
+//         );
+
+//         if (doesItemExist > -1) {
+//           //Item already exists in cart (change quantity)
+//           doesCartExist.products[doesItemExist].quantity -= quantity;
+//           console.log(doesCartExist.products[doesItemExist].quantity);
+//           if (doesCartExist.products[doesItemExist].quantity == 0) {
+//             doesCartExist.products.splice(doesItemExist, 1);
+//             console.log("splice", doesCartExist);
+//           }
+//         }
+//       } else {
+//         //item doesn't exist in cart
+//         return res.status(400).json({ errors: "item doesn't exist in cart" });
+//       }
+//       await doesCartExist.save();
+//       res.json(doesCartExist);
+//     } catch (error) {
+//       console.error(error.message);
+//       res.status(500).send("something went wrong :(");
+//     }
+//   }
+// );
+
+//@route    DELETE api/shop
+//@desc     Clear the cart
+//@access   Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    const doesCartExist = await Cart.findOne({ userId: req.user.id });
+    console.log(req.user.id);
+
+    // check if cart exists
+    if (!doesCartExist) {
+      return res.status(404).json({ msg: "Cart doesn't exist" });
+    }
+
+    await doesCartExist.remove();
+    res.json({ msg: "Cart removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json("Server Error");
+  }
+});
 
 module.exports = router;
