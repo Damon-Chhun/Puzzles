dbe2d183ab1e267619708b4395a44fa6eb383db5
Damon Chhun <damonchhun7@gmail.com>
2021-01-14 01:41:01 -0800

    quick console log

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,7 +12,6 @@ const bcrypt = require("bcryptjs");
 //@access   Private
 router.get("/", auth, async (req, res) => {
   try {
-    console.log(typeof req.user.id);
     const user = await User.findById(req.user.id).select("-password");
     console.log(user);
     res.json(user);
