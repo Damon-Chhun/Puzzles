cd6a89dd2e836ff6ea45dd1f808791807dc546d2
Damon Chhun <damonchhun7@gmail.com>
2021-01-14 13:12:11 -0800

    adding multiple routes to express routing

--- routes/api/shop.js
+++ routes/api/shop.js
@@ -11,7 +11,7 @@ const { check, validationResult } = require("express-validator");
 //@desc     Add products to Cart / create new Cart
 //@access   Private
 router.post(
-  "/",
+  ["/", "/:Department", "/:Department/:productID"],
   auth,
   [
     check("productID", "Invalid productID").isString(),
@@ -91,78 +91,29 @@ router.post(
   }
 );
 
-//@route    POST api/shop
-//@desc     Reduce quantity of an item
-//@access   Private
-// router.post(
-//   "/",
-//   [
-//     auth,
-//     [
-//       check("productID", "Invalid productID").exists(),
-//       check("quantity", "No quantity provided").isNumeric()
-//     ]
-//   ],
-//   async (req, res) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() });
-//     }
-
-//     console.log(req.body);
-
-//     const { productID, quantity } = req.body;
-//     const { id } = req.user;
-//     try {
-//       let doesCartExist = await Cart.findOne({ userId: id });
-
-//       // If there is already a cart that exists
-//       if (doesCartExist) {
-//         let doesItemExist = doesCartExist.products.findIndex(
-//           product => product.productID == productID
-//         );
-
-//         if (doesItemExist > -1) {
-//           //Item already exists in cart (change quantity)
-//           doesCartExist.products[doesItemExist].quantity -= quantity;
-//           console.log(doesCartExist.products[doesItemExist].quantity);
-//           if (doesCartExist.products[doesItemExist].quantity == 0) {
-//             doesCartExist.products.splice(doesItemExist, 1);
-//             console.log("splice", doesCartExist);
-//           }
-//         }
-//       } else {
-//         //item doesn't exist in cart
-//         return res.status(400).json({ errors: "item doesn't exist in cart" });
-//       }
-//       await doesCartExist.save();
-//       res.json(doesCartExist);
-//     } catch (error) {
-//       console.error(error.message);
-//       res.status(500).send("something went wrong :(");
-//     }
-//   }
-// );
-
 //@route    DELETE api/shop
 //@desc     Clear the cart
 //@access   Private
-router.delete("/", auth, async (req, res) => {
-  try {
-    const doesCartExist = await Cart.findOne({ userId: req.user.id });
-    console.log(req.user.id);
+router.delete(
+  ["/", "/:Department", "/:Department/:productID"],
+  auth,
+  async (req, res) => {
+    try {
+      const doesCartExist = await Cart.findOne({ userId: req.user.id });
+      console.log(req.user.id);
 
-    // check if cart exists
-    if (!doesCartExist) {
-      return res.status(404).json({ msg: "Cart doesn't exist" });
+      // check if cart exists
+      if (!doesCartExist) {
+        return res.status(404).json({ msg: "Cart doesn't exist" });
+      }
+
+      await doesCartExist.remove();
+      res.json({ msg: "Cart removed" });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json("Server Error");
     }
-
-    await doesCartExist.remove();
-    res.json({ msg: "Cart removed" });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).json("Server Error");
   }
-});
+);
 
 module.exports = router;
