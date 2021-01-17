02bb1325a56241f399829bfec55606cf29e3b186
Damon Chhun <damonchhun7@gmail.com>
2021-01-16 18:37:03 -0800

    Auth Controller implemented

---
+++ controllers/Auth.Controller.js
@@ -0,0 +1,66 @@
+const User = require("../models/User");
+const { check, validationResult } = require("express-validator");
+const jsonwebtoken = require("jsonwebtoken");
+const config = require("config");
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  getProfile: async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      console.log(user);
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  },
+  Login: async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      console.log(user);
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jsonwebtoken.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.rror(error.essage);
+      res.status(500).send("Server Error");
+    }
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,25 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
-const User = require("../../models/User");
+
+const AuthController = require("../../controllers/Auth.Controller");
+
 const { check, validationResult } = require("express-validator");
-const jsonwebtoken = require("jsonwebtoken");
-const config = require("config");
-const bcrypt = require("bcryptjs");
 
 //@route    GET api/auth
-//@desc      Test route
+//@desc     Get profile route
 //@access   Private
-router.get("/", auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select("-password");
-    console.log(user);
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
+router.get("/", auth, AuthController.getProfile);
 
 //@route    POST api/auth
 //@desc     Login a user
@@ -30,54 +20,7 @@ router.post(
     check("email", "Please include a valid email").isEmail(),
     check("password", "Password is invalid").exists()
   ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    console.log(req.body);
-
-    const { email, password } = req.body;
-
-    try {
-      let user = await User.findOne({ email });
-
-      console.log(user);
-
-      if (!user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "Invalid Credentials" }] });
-      }
-
-      const isMatch = await bcrypt.compare(password, user.password);
-
-      if (!isMatch) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "Invalid Credentials" }] });
-      }
-
-      const payload = {
-        user: {
-          id: user.id
-        }
-      };
-
-      jsonwebtoken.sign(
-        payload,
-        config.get("jwtSecret"),
-        { expiresIn: 3600 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (error) {
-      console.rror(error.essage);
-      res.status(500).send("Server Error");
-    }
-  }
+  AuthController.Login
 );
 
 module.exports = router;
