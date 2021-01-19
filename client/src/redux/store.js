1298954cf9571edb8c4965968e605dd937387417
Damon Chhun <damonchhun7@gmail.com>
2021-01-19 13:53:38 -0800

    redux-thunk

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,10 +2,11 @@ import { createStore, applyMiddleware } from "redux";
 //import { persistStore } from "redux-persist";
 import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [logger, thunk];
 
 export const store = createStore(
   rootReducer,
