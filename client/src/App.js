c86916ee73ff660f3f1107ba302649451a03de38
Damon Chhun <damonchhun7@gmail.com>
2021-01-19 16:06:38 -0800

    react-router added and hompage component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div>
+      <Route exact path="/" component={HomePage} />
+    </div>
+  );
 }
 
 export default App;
