fe15fdef083d04b997a843a7c7b58ed6041727ed
Damon Chhun <damonchhun7@gmail.com>
2021-07-09 14:12:25 -0700

    adding console.log in db.js

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
+    console.log("connecting to mongo...");
     await mongoose.connect(db, {
       useNewUrlParser: true,
 
