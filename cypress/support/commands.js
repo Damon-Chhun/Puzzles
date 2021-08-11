748c700087c23c946e20b99b4b2582c73b2035f0
Damon Chhun <damonchhun7@gmail.com>
2021-08-11 16:23:16 -0700

    added a createUser command for the login e2e

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,11 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { buildUser } from "./generate";
+
+Cypress.Commands.add("createUser", overrides => {
+  const user = buildUser();
+  console.log(user);
+  cy.request({
+    url: "http://localhost:5000/api/users/",
+    method: "POST",
+    body: user
+  }).then(response => ({ ...response.body.user, ...user }));
+});
